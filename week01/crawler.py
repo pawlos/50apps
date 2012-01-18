5bcab01cf0fc8849388cb83cf63faf306065a383
Pawel Lukasik <lukasik.pawel+github@gmail.com>
2012-01-19 00:59:29 +0100

    Additional parameter added that filters outputted url's only to those containing specified string

--- week01/crawler.py
+++ week01/crawler.py
@@ -6,18 +6,22 @@ from bs4 import BeautifulSoup
 
 visitedUrls = []
 
-def crawl(url, depth, keyword):	
+def crawl(url, depth, keyword, substring = ''):		
 	"""crawls web page, finds all links and check them recurevly"""
 	if (depth <= 0):
 		return
-	#print('Checking page (',depth,'): ', url)
+	print('Checking page (',depth,'): ', url)
 	visitedUrls.append(url)
 				
 	try:
+		#get page
 		page = urllib.request.urlopen(url)
 		soup = BeautifulSoup(page.read())			
+		#check if page content contains our keword
 		if (soup.find(text=re.compile(keyword))):
-			print('-->', url)	
+			#print(substring, url)
+			if ((len(substring) == 0) or (url.contains(substring))):
+				print('-->', url)	
 		#find links on the page	
 		anchors = soup.findAll('a', href=not '')		
 		#spawn more crawlers
@@ -27,12 +31,12 @@ def crawl(url, depth, keyword):
 				continue
 			if (a['href'] in visitedUrls):
 				continue
-			crawl(a['href'], depth-1,keyword)
+			crawl(a['href'], depth-1,keyword, substring)
 	except KeyboardInterrupt:
-		pass
-	except (html.parser.HTMLParseError, urllib.error.HTTPError, 
-		    urllib.error.URLError, ValueError, http.client.BadStatusLine,
-		    TypeError):
+		print("Breaking...")
+		sys.exit(-1)
+		return
+	except:
 		return
 
-crawl('http://www.hackernews.com', 5, 'python')
+crawl('http://www.hackernews.com', 5, 'python', substring='hackernews')
