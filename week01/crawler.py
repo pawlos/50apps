c3f129e5dbcadf3024341a4fc0efcb5a92a30807
Pawel Lukasik <lukasik.pawel+github@gmail.com>
2012-01-21 00:56:25 +0100

    New version. Removed soup. Code refactoring

--- week01/crawler.py
+++ week01/crawler.py
@@ -1,42 +1,58 @@
-import urllib.request
-import html.parser
 import http.client
+import urllib.parse
+import sys
 import re
-from bs4 import BeautifulSoup
+
+startUrl = 'http://www.hackernews.net'
+depth = 2
+keyword = 'python'
+substringInUrl = ''
 
 visitedUrls = []
 
 def crawl(url, depth, keyword, substring = ''):		
 	"""crawls web page, finds all links and check them recurevly"""
-	if (depth <= 0):
+	if (depth < 0):
 		return
-	print('Checking page (',depth,'): ', url)
+		
+	if ('http' not in url):
+		url = 'http://'+url
+	#print('Checking page (',depth,'):', url)	
+		
 	visitedUrls.append(url)
-				
-	try:
-		#get page
-		page = urllib.request.urlopen(url)
-		soup = BeautifulSoup(page.read())			
-		#check if page content contains our keword
-		if (soup.find(text=re.compile(keyword))):
-			#print(substring, url)
-			if ((len(substring) == 0) or (url.contains(substring))):
-				print('-->', url)	
-		#find links on the page	
-		anchors = soup.findAll('a', href=not '')		
-		#spawn more crawlers
-		for a in anchors:		
-			#print(a)
-			if (not a['href'].startswith('http')):
-				continue
-			if (a['href'] in visitedUrls):
-				continue
-			crawl(a['href'], depth-1,keyword, substring)
-	except KeyboardInterrupt:
-		print("Breaking...")
-		sys.exit(-1)
+	
+	address = urllib.parse.urlparse(url)		
+	#print(address)
+	connection = http.client.HTTPConnection(address[1])	
+	request = connection.request("GET", address[2]+address[3])
+	response = connection.getresponse();
+	if response.status in (301,302,):
+		#handle redirects				
+		url = urllib.parse.urljoin(url, response.getheader('location', ''))		
+		crawl(url, depth-1, keyword, substring) #should it be one level deeper?
 		return
-	except:
-		return
-
-crawl('http://www.hackernews.com', 5, 'python', substring='hackernews')
+	
+	content = response.read()		
+	connection.close()
+	#check if page content contains our keword
+	matches = re.findall(b'href="http://(.+?)"', content)		
+	if re.search(keyword.encode(), content):
+		if (len(substring) == 0) or (substring in url):
+			print('--> Keyword ',keyword,' found on: ', url)	
+			
+	#spawn more crawlers
+	for href in matches:
+		pageToVisit = href.decode()		
+		if (pageToVisit not in visitedUrls):
+			crawl(pageToVisit, depth-1,keyword, substring)			
+			
+if (len(sys.argv) > 1):
+	startUrl = sys.argv[1]
+if (len(sys.argv) > 2):
+	depth = int(sys.argv[2])
+if (len(sys.argv) > 3):
+	keyword = sys.argv[3]
+if (len(sys.argv) > 4):
+	substringInUrl = sys.argv[4]
+	
+crawl(startUrl, depth, keyword, substringInUrl)
