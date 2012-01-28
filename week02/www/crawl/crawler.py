fb97e832402fec6e60002861033a91b351691bd2
Pawel Lukasik <lukasik.pawel+github@gmail.com>
2012-01-28 23:28:07 +0100

    Crawler changes to return urls in one of the parameters. Displaying links on page.

--- week02/www/crawl/crawler.py
+++ week02/www/crawl/crawler.py
@@ -7,39 +7,47 @@ import re
 
 visitedUrls = []
 
-def crawl(url, depth, keyword, substring = ''):		
+def crawl(url, depth, keyword, urls):		
 	"""crawls web page, finds all links and check them recurevly"""
-	if (depth < 0):
+	if depth < 0:
 		return
 		
-	if ('http' not in url):
+	if 'http' not in url:
 		url = 'http://'+url	
-		
-	visitedUrls.append(url)
-	
-	address = urlparse(url)		
-	#print(address)
-	connection = httplib.HTTPConnection(address[1])	
-	request = connection.request("GET", address[2]+address[3])
-	response = connection.getresponse();
-	if response.status in (301,302,):
-		#handle redirects				
-		url = urljoin(url, response.getheader('location', ''))		
-		crawl(url, depth-1, keyword, substring) #should it be one level deeper?
+	if url in visitedUrls:
 		return
 	
-	content = response.read()		
-	connection.close()
-	#check if page content contains our keword
-	matches = re.findall(b'href="http://(.+?)"', content)		
-	if re.search(keyword.encode(), content):
-		if (len(substring) == 0) or (substring in url):
-			print '--> Keyword ',keyword,' found on: ', url
-			
-	#spawn more crawlers
-	for href in matches:
-		pageToVisit = href.decode()		
-		if (pageToVisit not in visitedUrls):
-			crawl(pageToVisit, depth-1,keyword, substring)			
-			
-crawl('http://www.wp.pl', 3, 'sport')
+	visitedUrls.append(url)
+	print 'crawling: ' + url
+	
+	address = urlparse(url)			
+	
+	try:
+		#print(address)
+		connection = httplib.HTTPConnection(address[1])	
+		request = connection.request("GET", address[2]+address[3])
+		response = connection.getresponse();
+		if response.status in (301,302,):
+			#handle redirects				
+			url = urljoin(url, response.getheader('location', ''))		
+			crawl(url, depth-1, keyword, urls) #should it be one level deeper?		
+	
+		content = response.read()		
+		connection.close()
+		#check if page content contains our keword
+		matches = re.findall(b'href="http://(.+?)"', content)	
+		if re.search(keyword.encode(), content):
+			print 'found ' + keyword + ' on page ' + url
+			urls.append({'url': url, 'depth': depth})
+				
+		if depth == 0:
+			return
+		print 'Found ', len(matches), ' additional links'
+		#spawn more crawlers
+		for href in matches:
+			pageToVisit = href.decode()		
+			if pageToVisit not in visitedUrls:
+				crawl(pageToVisit, depth-1,keyword, urls)
+	except:
+		print sys.exc_info()[0]
+		pass
--- week02/www/crawl/views.py
+++ week02/www/crawl/views.py
@@ -1,7 +1,7 @@
 from django.core.context_processors import csrf
 from django.http import HttpResponse
 from django.template import Context, loader
-import crawler
+from crawler import *
 
 def index(request):
 	t = loader.get_template('index.html')
@@ -10,12 +10,16 @@ def index(request):
 	return HttpResponse(t.render(c))
 
 def go(request):
+	if ('url' not in request.POST):
+		return index(request)
+	
 	url = request.POST['url']
 	depth = request.POST['depth']
 	phrase = request.POST['phrase']
-	crawl(url, depth, phrase)
+	urls = []
+	crawl(url, int(depth), phrase, urls)
 	
 	t = loader.get_template('index.html')
-	c = Context({ 'url': url, 'depth': depth, 'phrase': phrase})	
+	c = Context({ 'url': url, 'depth': depth, 'phrase': phrase, 'urls': urls})	
 	c.update(csrf(request))
 	return HttpResponse(t.render(c))	
