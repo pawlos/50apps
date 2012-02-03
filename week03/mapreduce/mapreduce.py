b9a5e28cfa5f82fc2fba05ff03d1158f7b92106c
Pawel Lukasik <lukasik.pawel+github@gmail.com>
2012-02-04 00:44:37 +0100

    map reduce demo.

---
+++ week03/mapreduce/mapreduce.py
@@ -0,0 +1,19 @@
+import functools
+
+def mapReduce(list):	
+	print(functools.reduce(lambda x,y: reduceFunc(x,y), map(lambda x: {x: 1}, filter(filterFunc, list))))
+		
+def reduceFunc(x,y):	
+	second = y.popitem()	
+	if second[0] in x:
+		x[second[0]] += second[1]
+	else:
+		x[second[0]] = second[1]
+	return x
+	
+def filterFunc(x):
+	return x not in ['a', 'an','the','on','in','for','and','to']
+
+
+list = ["two","acta","acta","bacta","a","the","two", "to",'single','most','most','most','most']
+mapReduce(list)
