d946668dde419da82c77f9daacc1b9b8f2c0e659
Pawel Lukasik <lukasik.pawel+github@gmail.com>
2012-02-25 07:30:58 +0100

    Initial commit

---
+++ week06/snake.js
@@ -0,0 +1,18 @@
+function snake() {
+}
+
+snake.prototype._ctx = null;
+snake.prototype._
+
+snake.prototype.init = function(ctx) {
+	this._ctx = ctx;
+}
+
+snake.prototype.draw = function() {
+}
+
+snake.prototype.move = function() {
+}
+
+snake.prototype.eat = function() {
+}
