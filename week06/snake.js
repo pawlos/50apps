379f8ed09807e46afa91dcdf4a7cbf755f3c7a94
Pawel Lukasik <lukasik.pawel+github@gmail.com>
2012-02-28 01:22:06 +0100

    Eating fruit functionality

--- week06/snake.js
+++ week06/snake.js
@@ -1,9 +1,11 @@
 var ctx;
 var head = {x: 0, y:0};
+var elems = [];
 var dir = {x: 0, y: 0};
 var canvas;
 var fruits = [];
 var cellSize = 12;
+var isRunning = false;
 
 jQuery(document).ready(function() {
 	canvas = jQuery('#snakeboard');
@@ -19,6 +21,8 @@ jQuery(document).ready(function() {
 			dir = {x:cellSize, y: 0};
 		} else if (key == 40) {
 			dir = {x:0, y: +cellSize};
+		} else if (key == 27) {
+			pause();
 		}
 	});	
 	head.x = Math.round((canvas.width() / 2 / cellSize))*cellSize;
@@ -26,20 +30,76 @@ jQuery(document).ready(function() {
 	setInterval(mainLoop, 200);
 });
 
-function advancePosition() {
+function pause() {
+	isRunning = !isRunning;
+}
+
+function advancePosition() {	
+	for (var i=elems.length-1; i>=0; i--) {		
+		if (i == 0) {		
+			elems[i].x = -dir.x;
+			elems[i].y = -dir.y;
+		}
+		else {		
+			elems[i].x = elems[i-1].x;
+			elems[i].y = elems[i-1].y;
+		}
+	}
 	head = { x: head.x + dir.x, y: head.y + dir.y};
 }
 
 function placeFruit() {
-	if (fruits.length > 1)
+	if (fruits.length > 0)
 		return;
-	var fruit = {x:0, y:0, type: 'lemon'};
-	fruit.x = Math.round((Math.random()*canvas.width() / cellSize))*cellSize;
-	fruit.y = Math.round((Math.random()*canvas.height() / cellSize))*cellSize;
+	var fruit = {x:0, y:0, type: getType()};
+	fruit.x = Math.round((Math.random()*(canvas.width() - cellSize) / cellSize))*cellSize;
+	fruit.y = Math.round((Math.random()*(canvas.height() - cellSize) / cellSize))*cellSize;
 	fruits.push(fruit);
 }
 
+function getType() {
+	var rand = Math.random()*2;
+	if (rand < 1)
+		return 'lemon';
+	else
+		return 'grapefruit';
+}
+
+function hitFruit() {
+	if (fruits.length == 0)
+		return false;
+	var leftCorX = fruits[0].x;
+	var leftCorY = fruits[0].y;
+	var headCorX = head.x + cellSize/2;
+	var headCorY = head.y + cellSize/2;
+	
+	return headCorX > leftCorX && headCorY > leftCorY && headCorX < leftCorX + cellSize*2 && headCorY < leftCorY + cellSize*2;
+}
+
+function makeSnakeBigger() {
+	delete fruits[0];
+	fruits.splice(0,1);
+	if (elems.length == 0)
+		elems.push({x: dir.x, y: dir.y});
+	else {
+		var last = elems[elems.length-1];
+		elems.push({x: last.x, y: last.y});
+	}
+}
+
+function drawCircle(x, y) {
+	ctx.beginPath();
+	ctx.arc(x, y, cellSize, 0, 2*Math.PI, false);
+	ctx.fillStyle = "#8ED6FF";
+	ctx.fill();
+	ctx.lineWidth = 3;
+	ctx.strokeStyle = "black";
+	ctx.stroke();
+}
+
 function mainLoop() {
+	if (!isRunning)
+		return;
 	advancePosition();
 	placeFruit();
 	//if (hitWall())
@@ -47,6 +107,9 @@ function mainLoop() {
 	//	gameOver();
 	//	clearInverval();
 	//}	
+	if (hitFruit()) {
+		makeSnakeBigger();
+	}
 	ctx.clearRect(0,0, canvas.width(), canvas.height());
 	//draw fruits
 	for(var id in fruits) 	{
@@ -54,11 +117,11 @@ function mainLoop() {
 		ctx.drawImage(jQuery('#'+fruit.type).get(0), fruit.x,fruit.y);
 	}
 	//draw snake	
-	ctx.beginPath();
-	ctx.arc(head.x, head.y, cellSize, 0, 2*Math.PI, false);
-	ctx.fillStyle = "#8ED6FF";
-	ctx.fill();
-	ctx.lineWidth = 3;
-	ctx.strokeStyle = "black";
-	ctx.stroke();
+	drawCircle(head.x, head.y);
+	var pos = {x: head.x, y: head.y};
+	for(var cell in elems) {
+		var c = elems[cell];
+		var pos = {x: pos.x + c.x, y: pos.y + c.y};
+		drawCircle(pos.x, pos.y);
+	}
 }
